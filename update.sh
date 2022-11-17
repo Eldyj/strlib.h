4f6175db3f88d66b2e897d5d0642bdbc49da7de0
Myshiak.su <67818235+Myshiak@users.noreply.github.com>
2022-11-17 18:15:59 +0300

    bash script for updating (the 7th commit)

--- update.sh
+++ update.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/bash
 echo "Getting versions..."
 version_web=$(curl https://raw.githubusercontent.com/Myshiak/strlib.h/main/strlib.h | grep STRLIB_VERSION | awk '{ print $3 }')
+if [[ -z $version_web ]]
+then
+	echo "Updating failed: invalid version on github"
+	exit 1
+fi
 vwlen=$(expr length "$version_web")
 version_web=${version_web:1:$((vwlen-2))}
 version_file=$(cat strlib.h | grep STRLIB_VERSION | awk '{ print $3 }')
+if [[ -z $version_file ]]
+then
+	echo "Updating failed: invalid version in the file 'strlib.h'"
+	exit 1
+fi
 vflen=$(expr length "$version_file")
 version_file=${version_file:1:$((vflen-2))}
 if [[ $version_file == $version_web ]]
@@ -19,21 +29,24 @@ else
 			chmod +x Makefile
 			trgos=$(uname -o)
 			fprefix="/usr/local"
-			[[ "$trgos" == Android ]] && fprefix=$PREFIX
+			sudo=sudo
+			[[ "$trgos" == Android ]] && fprefix=$PREFIX && sudo=
 			if [[ -f $fprefix/lib/libstrlib.a ]] && [[ -f $fprefix/include/strlib.h ]]
 			then
-				if [[ "$trgos" == Android ]]
+				if [[ -f libstrlib.a ]] && [[ -f strlib.o ]]
 				then
-					make uninstall
-					make install
-				else
-					sudo make uninstall
-					sudo make install
+					clean=clean
 				fi
+				$sudo make $clean reinstall
 			fi
 			version_file=$(cat strlib.h | grep STRLIB_VERSION | awk '{ print $3 }')
-			vflen=$(expr length "$version_file")
-			version_file=${version_file:1:$((vflen-2))}
+			if [[ -n $version_file ]]
+			then
+				vflen=$(expr length "$version_file")
+				version_file=${version_file:1:$((vflen-2))}
+			else
+				false
+			fi
 		}
     } && echo "Successfully updated to version $version_file"
 fi
